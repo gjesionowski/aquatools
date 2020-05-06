33be31db5dca151910cb72f1322ebfac44a8a14f
gjesionowski <gregory.a.jesionowski@gmail.com>
2020-05-06 16:16:57 -0700

    Link to tools page created and functioning

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace AquaTools.Controllers
             return View();
         }
 
+        public IActionResult Tools()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
